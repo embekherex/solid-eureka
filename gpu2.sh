c61383a76096260755272a9558bdd800ed7f2108
embekherex <85697607+embekherex@users.noreply.github.com>
2021-06-11 01:25:54 +0800

    Delete gpu2.sh

--- gpu2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-POOL=etc.2miners.com:1010
-WALLET=0x8d13ACC41d46327A30adF7B2D4132D16e82f57c9
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-EMBEKHEREX)
-chmod +x tuyulgpu
-./tuyulgpu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER
